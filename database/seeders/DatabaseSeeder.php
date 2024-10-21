b8177c91e6ab0e0a166440b6da5ca0b9705b8c15
Anderson Bispo <andersonsbispo1997@outlook.com>
2024-10-20 21:00:12 -0300

    creating project and user tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,17 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Project;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    public function run(): void {
+        User::factory()->count(200)->create();
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id]));
     }
 }
